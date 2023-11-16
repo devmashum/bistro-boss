1b05b3e2b065f4b4249dfc551f2455454783c25f
devmashum <dev.mashum@gmail.com>
2023-11-16 23:02:00 +0100

    my cart page has been designed

--- src/Dashboard/Cart.jsx
+++ src/Dashboard/Cart.jsx
@@ -1,9 +1,97 @@
+import { FaTrash } from "react-icons/fa";
+import useCart from "../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 
 const Cart = () => {
+
+    const [cart, refetch] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelet = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+
+            }
+        });
+    }
+
     return (
         <div>
-            <h2 className="text-6xl">My Cart</h2>
+            <div className="flex justify-evenly items-center mt-10 bg-blue-100 p-10">
+                <h2 className="text-2xl">Total Items: {cart.length}</h2>
+                <h2 className="text-2xl">Total Price: {totalPrice}</h2>
+                <button className="btn btn-secondary text-2xl">Pay</button>
+            </div>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead className="text-xl font-bold">
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th className="text-xl">
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td>
+                                    <h3 className="text-xl">    {item.name}</h3>
+                                </td>
+                                <td className="text-xl">$ {item.price}</td>
+                                <th>
+                                    <button onClick={() => handleDelet(item._id)} className="btn text-xl   btn-base">
+                                        <FaTrash></FaTrash>
+                                    </button>
+                                </th>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,20 +1,23 @@
 
 import { NavLink, Outlet } from 'react-router-dom';
 import { MdAddShoppingCart, MdBook, MdRateReview } from "react-icons/md";
-import { FaCalendar, FaHome } from "react-icons/fa";
+import { FaBook, FaCalendar, FaHome } from "react-icons/fa";
+import useCart from '../hooks/useCart';
 
 const Dashboard = () => {
+
+    const [user] = useCart();
     return (
         <div className='flex gap-2'>
-            <div className="w-64 min-h-screen  bg-blue-300 ">
-                <ul className='menu '>
+            <div className="w-64 min-h-screen bg-blue-300 ">
+                <ul className='menu text-xl font-bold '>
                     <li><NavLink to={'/dashboard/userHome'}><FaHome />User Home</NavLink></li>
 
                     <li><NavLink to={'/dashboard/reservation'}>
                         <FaCalendar></FaCalendar>
                         Reservation</NavLink></li>
 
-                    <li><NavLink to={'/dashboard/cart'}><MdAddShoppingCart />My Cart</NavLink></li>
+                    <li><NavLink to={'/dashboard/cart'}><MdAddShoppingCart />My Cart <span className='text-red-600'>({user.length})</span></NavLink></li>
 
                     <li><NavLink to={'/dashboard/review'}>
                         <MdRateReview></MdRateReview>
@@ -23,6 +26,15 @@ const Dashboard = () => {
                     <li><NavLink to={'/dashboard/bookings'}>
                         <MdBook></MdBook>
                         My Bookings</NavLink></li>
+
+                    <div className="divider"></div>
+
+                    <li><NavLink to={'/'}><FaHome />Home</NavLink></li>
+                    <li><NavLink to={'/order/salad'}>
+                        <FaBook />
+
+                        Menu</NavLink></li>
+
 
                 </ul>
             </div>
