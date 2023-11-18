c21dcc1aaf582cacbe1e9536cf749552d2cfca30
devmashum <dev.mashum@gmail.com>
2023-11-18 23:56:02 +0100

    new commit

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,13 @@ const axiosSecure = axios.create({
     baseURL: 'http://localhost:3000/'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        console.log('request stopped by interceptors', token)
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    })
     return axiosSecure;
 };
 
